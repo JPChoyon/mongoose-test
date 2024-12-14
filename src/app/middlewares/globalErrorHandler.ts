9c3f495cd019968ad14b98ae90ffb16f4d86f2c6
JP Choyon Khan <kotjpckhan@gmail.com>
2024-12-14 21:36:25 +0600

    part 3 middlewares and app error

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,5 +1,7 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
 /* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { NextFunction, Request, Response } from 'express';
 
 const globalErrorHandler = (
@@ -8,7 +10,7 @@ const globalErrorHandler = (
   res: Response,
   next: NextFunction,
 ) => {
-  const statusCode = 500;
+  const statusCode = err.statusCode || 500;
   const message = err.message || 'Something went wrong!';
 
   return res.status(statusCode).json({
